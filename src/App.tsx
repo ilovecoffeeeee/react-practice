29184dce83c12d21ff87c5f65ea4660abc3b59db
sun_loves_Q <conconxel@gmail.com>
2022-03-01 02:51:05 +0900

    Setup
    npx create-react-app react-practice --template typescript
    npm i styeld-component
    npm i react-router-dom@5.3.0
    npm i react-query
    npm i gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const Title = styled.div`
+  color: red;
+`;
+
+const ToDos = styled.ul`
+  
+`;
+
+const Todo = styled.li`
+  
+`;
+
+const toDoList = [
+  "npx create-react-app react-practice --template typescript",
+  "npm i styeld-component",
+  "npm i react-router-dom@5.3.0",
+  "npm i react-query",
+  "npm i gh-pages",
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Title>Set up</Title> 
+      <ToDos>
+          {toDoList.map((todo) => <Todo>{todo}</Todo>)}
+      </ToDos>
+    </Container>
+    
   );
 }
 
