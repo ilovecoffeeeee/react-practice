ba2a7732716f9d4685dee1297d9bf296c6cc341e
sun_loves_Q <conconxel@gmail.com>
2022-03-10 01:31:04 +0900

    practice atom and selector

--- src/routes/ToDoList.tsx
+++ src/routes/ToDoList.tsx
@@ -1,8 +1,24 @@
+import { useRecoilState, useRecoilValue } from "recoil";
+import { minuteState, hourSelector } from "./atoms";
+
 export function ToDoList () {
+    const [minutes, setMinutes] = useRecoilState(minuteState);
+    const [hours, setHours] = useRecoilState(hourSelector);
+    const onMinutesChange = (event:React.FormEvent<HTMLInputElement>) => {
+        setMinutes(+event.currentTarget.value);
+    };
+    const onHoursChange = (event:React.FormEvent<HTMLInputElement>) => {
+        setHours(+event.currentTarget.value);
+    }
     return (
-        <div>To Do List</div>
+        <div>
+            <input value={minutes} onChange={onMinutesChange} type="number" placeholder="Minutes" />
+            <input value={hours} onChange={onHoursChange} type="number" placeholder="Hours" />
+        </div>
     );
 };
 
+
+export default ToDoList;
 
 
