3038aebc14c658802fcdad19c9a24a84e9f13b49
sun_loves_Q <conconxel@gmail.com>
2022-03-10 23:21:15 +0900

    using react memo

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,69 @@
-import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import { DragDropContext, Draggable, Droppable, DropResult} from "react-beautiful-dnd";
+import { useRecoilState } from "recoil";
+import styled from "styled-components";
+import { toDoState } from "./atoms";
+import DragabbleCard from "./Components/DragabbleCard";
+
+
+const Wrapper = styled.div`
+  display: flex;
+  max-width: 480px;
+  width: 100%;
+  margin: 0 auto;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const Boards = styled.div`
+  display:grid;
+  width: 100%;
+  grid-template-columns:repeat(1, 1fr);
+`;
+
+const Board = styled.div`
+  padding: 20px 10px;
+  padding-top: 30px;
+  background-color: ${props => props.theme.boardColor};
+  border-radius: 5px;
+  min-height: 300px;
+`;
+
+
 
 function App() {
-  const onDragEnd = () => {}
+  const [toDos, setToDos] = useRecoilState(toDoState)
+  const onDragEnd = ({draggableId, destination, source}:DropResult) => {
+    if(!destination) return;
+
+    setToDos((oldToDos) => {
+      const toDosCopy = [...oldToDos];
+      console.log("deleted iten on", source.index);
+      console.log(toDosCopy);
+      toDosCopy.splice(source.index, 1)
+      console.log("delete");
+      console.log(toDosCopy);
+      console.log("Put back", draggableId, "on", destination.index);
+      toDosCopy.splice(destination?.index, 0, draggableId);
+      console.log(toDosCopy);
+      return toDosCopy
+    })
+  };
   return (
     <DragDropContext onDragEnd={onDragEnd}>
-      <div>
-        <Droppable droppableId="one">
-          {(provided) => 
-          <ul ref={provided.innerRef} {...provided.droppableProps}>
-            <Draggable draggableId="first" index={0}>
-              {(provided) => (
-                <li
-                  ref={provided.innerRef}
-                  {...provided.draggableProps}
-                  {...provided.dragHandleProps}
-                >
-                  <span {...provided.dragHandleProps}>💥</span>
-                  One
-                </li>
+      <Wrapper>
+        <Boards>
+          <Droppable droppableId="one">
+            {(provided) => 
+            <Board ref={provided.innerRef} {...provided.droppableProps}>
+              {toDos.map((toDo, index) => 
+                <DragabbleCard key={toDo} index={index} toDo={toDo} />
               )}
-            </Draggable>
-            </ul>}
-        </Droppable>
-      </div>
+              {provided.placeholder}
+              </Board>}
+          </Droppable>
+        </Boards>
+      </Wrapper>
     </DragDropContext>
   );
 }
