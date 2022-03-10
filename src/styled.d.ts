421df6a008adc2ec709227d08857ec9453a8f993
sun_loves_Q <conconxel@gmail.com>
2022-03-11 00:47:20 +0900

    style copy

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -4,6 +4,6 @@ declare module "styled-components" {
   export interface DefaultTheme {
     bgColor: string;
     boardColor: string;
-    cardBgColor: string;
+    cardColor: string;
   }
 }
--- src/theme.ts
+++ src/theme.ts
@@ -3,5 +3,5 @@ import { DefaultTheme } from "styled-components";
 export const darkTheme: DefaultTheme = {
   bgColor: "#3F8CF2",
   boardColor: "#DADFE9",
-  cardBgColor: "white",
+  cardColor: "white",
 };
