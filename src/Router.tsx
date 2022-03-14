c985a980a6733e323ef8caaa2a0d8ba84ce8c778
sun_loves_Q <conconxel@gmail.com>
2022-03-14 21:39:40 +0900

    add Tabs on the Top

--- src/Router.tsx
+++ src/Router.tsx
@@ -7,24 +7,52 @@ interface IRouterProps {
 
 };
 
+const Tabs = styled.div`
+    width: 100%;
+    height: 10%;
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: center;
+    padding: 5px;
+    background-color: black;
+`;
+
+
 const Tab = styled.div`
     width: 100px;
-    height: 100px;
+    height: 30px;
+    padding-top: 5px; 
+    margin-right: 10px;
+    background-color: whitesmoke;
+    border-radius: 10px;
+    text-align: center;
 `;
 
 function Router({}:IRouterProps) {
     return (        
         <BrowserRouter>
-            <Tab>
-                <Link to={`/result/todolist`}>To Do List</Link>
-            </Tab>
+            <Tabs>
+                <Tab>
+                    <Link to={`/`}>Home</Link>
+                </Tab>
+                <Tab>
+                    <Link to={`/practice/netflix`}>Netflix Clone</Link>
+                </Tab>
+                <Tab>
+                    <Link to={`/practice/todolist`}>To Do List</Link>
+                </Tab>
+            </Tabs>
             <Switch>
+                <Route path="/practice/netfilx">
+
+                </Route>
+                <Route path="/practice/todolist">
+                    <ToDoList />
+                </Route>
                 <Route path="/">
                     <Home />
                 </Route >
-                <Route path="/result/todolist">
-                    <ToDoList />
-                </Route>
             </Switch>
         </BrowserRouter>
     
